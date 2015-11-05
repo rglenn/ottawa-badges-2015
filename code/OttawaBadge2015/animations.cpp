df57cd4bbaa094aa533e7607af82cc9febf0549c
Randy Glenn <randy@surrealitylabs.com>
2015-11-05 15:16:00 -0500

    Finished animations.

--- code/OttawaBadge2015/animations.cpp
+++ code/OttawaBadge2015/animations.cpp
@@ -8,7 +8,8 @@ uint32_t ledChaseUp(uint8_t *animationFrameNumber) {
   
   actualFrameNumber = (*animationFrameNumber) / 2;
 
-  frame = (1 << actualFrameNumber);
+  frame = 1;
+  frame <<= actualFrameNumber;
 
   (*animationFrameNumber)++;
   (*animationFrameNumber) %= 40; // rollover at 20 updates
@@ -22,7 +23,8 @@ uint32_t ledChaseDown(uint8_t *animationFrameNumber) {
   
   actualFrameNumber = (*animationFrameNumber) / 2;
 
-  frame = (1 << (19 - actualFrameNumber));
+  frame = 1;
+  frame <<= (19 - actualFrameNumber);
 
   (*animationFrameNumber)++;
   (*animationFrameNumber) %= 40; // rollover at 20 updates
@@ -36,11 +38,13 @@ uint32_t ledChaseUpDown(uint8_t *animationFrameNumber) {
 
   actualFrameNumber = (*animationFrameNumber) / 2;
 
+  frame = 1;
+
   if((*animationFrameNumber) <= 40) {
-    frame = (1 << actualFrameNumber);
+    frame <<= actualFrameNumber;
   } else {
-    actualFrameNumber -= 20;
-    frame = (1 << (19 - actualFrameNumber));
+    actualFrameNumber -= 21;
+    frame <<= (19 - actualFrameNumber);
   }
 
   (*animationFrameNumber)++;
@@ -56,16 +60,17 @@ uint32_t ledChaseAround(uint8_t *animationFrameNumber) {
 
   actualFrameNumber = (*animationFrameNumber) / 2;
 
-  if((*animationFrameNumber) <= 40) {
+  frame = 1;
+
+  if((actualFrameNumber) < 10) {
     ledNumber = actualFrameNumber * 2;
-    frame = (1 << ledNumber);
   } else {
-    ledNumber = 40 - (actualFrameNumber * 2);
-    frame = (1 << ledNumber);
+    ledNumber = 20-(2*(actualFrameNumber - 10)) - 1;
   }
 
+  frame <<= ledNumber;
   (*animationFrameNumber)++;
-  (*animationFrameNumber) %= 80; // rollover at 40 updates
+  (*animationFrameNumber) %= 40; // rollover at 40 updates
 
   return frame;
 }
@@ -92,9 +97,11 @@ uint32_t ledDoubleUp(uint8_t *animationFrameNumber) {
   uint8_t actualFrameNumber;
   uint32_t frame;
   
-  actualFrameNumber = (*animationFrameNumber) / 2;
+  actualFrameNumber = (*animationFrameNumber) / 4;
+  actualFrameNumber *= 2;
 
-  frame = (1 << actualFrameNumber) | (1 << (actualFrameNumber + 1));
+  frame = 3;
+  frame <<= actualFrameNumber;
 
   (*animationFrameNumber) += 2;
   (*animationFrameNumber) %= 40; // rollover at 40 updates
@@ -106,9 +113,11 @@ uint32_t ledDoubleDown(uint8_t *animationFrameNumber) {
   uint8_t actualFrameNumber;
   uint32_t frame;
   
-  actualFrameNumber = (*animationFrameNumber) / 2;
+  actualFrameNumber = (*animationFrameNumber) / 4;
+  actualFrameNumber *= 2;
 
-  frame = (1 << (19 - actualFrameNumber)) | (1 << ((19 - actualFrameNumber) + 1));
+  frame = 3;
+  frame <<= (19 - actualFrameNumber)-1;
 
   (*animationFrameNumber) += 2;
   (*animationFrameNumber) %= 40; // rollover at 40 updates
@@ -144,12 +153,12 @@ uint32_t ledLarsonTogether(uint8_t *animationFrameNumber) {
   uint32_t frame;
   uint8_t actualFrameNumber;
 
-  actualFrameNumber = (*animationFrameNumber) / 2;
+  actualFrameNumber = (*animationFrameNumber);
 
   frame = pgm_read_dword_near(larsonPattern + actualFrameNumber);
 
   (*animationFrameNumber)++;
-  (*animationFrameNumber) %= (LARSON_STEPS * 2); // rollover at 40 updates
+  (*animationFrameNumber) %= (LARSON_STEPS); // rollover at 40 updates
 
   return frame;
 }
@@ -182,12 +191,12 @@ uint32_t ledLarsonOpposite(uint8_t *animationFrameNumber) {
   uint32_t frame;
   uint8_t actualFrameNumber;
 
-  actualFrameNumber = (*animationFrameNumber) / 2;
+  actualFrameNumber = (*animationFrameNumber);
 
   frame = pgm_read_dword_near(oppositeLarsonPattern + actualFrameNumber);
 
   (*animationFrameNumber)++;
-  (*animationFrameNumber) %= (OPPOSITE_LARSON_STEPS * 2); // rollover at 40 updates
+  (*animationFrameNumber) %= (OPPOSITE_LARSON_STEPS); // rollover at 40 updates
 
   return frame;
 }
@@ -228,37 +237,37 @@ uint32_t doAnimation(uint8_t animationNumber, uint8_t *animationFrameNumber) {
       return 0;
       break;
     case 1:
-      return ledChaseUp(animationFrameNumber);
+      return ledChaseUp(animationFrameNumber); // works
       break;
     case 2:
-      return ledChaseDown(animationFrameNumber);
+      return ledChaseDown(animationFrameNumber); // works
       break;
     case 3:
-      return ledChaseUpDown(animationFrameNumber);
+      return ledChaseUpDown(animationFrameNumber); // works
       break;
     case 4:
-      return ledChaseAround(animationFrameNumber);
+      return ledChaseAround(animationFrameNumber); // works
       break;
     case 5:
-      return ledMarquee(animationFrameNumber);
+      return ledMarquee(animationFrameNumber); // works
       break;
     case 6:
-      return ledDoubleUp(animationFrameNumber);
+      return ledDoubleUp(animationFrameNumber); // works
       break;
     case 7:
-      return ledDoubleDown(animationFrameNumber);
+      return ledDoubleDown(animationFrameNumber); // works
       break;
     case 8:
-      return ledLarsonTogether(animationFrameNumber);
+      return ledLarsonTogether(animationFrameNumber); // works
       break;
     case 9:
-      return ledLarsonOpposite(animationFrameNumber);
+      return ledLarsonOpposite(animationFrameNumber); // works
       break;
     case 10:
-      return ledBlink(animationFrameNumber);
+      return ledBlink(animationFrameNumber); // works
       break;
     case 11:
-      return ledBlinkAlternate(animationFrameNumber);
+      return ledBlinkAlternate(animationFrameNumber); // works
       break;
     case 12:
     case 13:
